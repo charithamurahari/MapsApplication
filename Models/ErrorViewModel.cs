543d04c9872cde1c9be3420562ec0420478ffb45
Murahari Charitha <murahari.charitha@signify.com>
2021-09-13 16:44:47 +0530

    Copying initial code files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MapsApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/OpenHibernateSession.cs
@@ -0,0 +1,54 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+
+namespace MapsApplication.Models
+{
+    public class OpenHibernateSession
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    var configuration = new Configuration();
+                    configuration.Configure();
+                    configuration.AddAssembly(typeof(AddLocation).Assembly);
+                    _sessionFactory = configuration.BuildSessionFactory();
+                }
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+        public class LocationRepo
+        {
+            public void Add(AddLocation addLocation)
+            {
+                using (ISession session = OpenHibernateSession.OpenSession())
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(addLocation);
+                        transaction.Commit();
+                    }
+                }
+            }
+            public AddLocation GetLocationName(string Name)
+            {
+                AddLocation addLocation = new AddLocation();
+                using (ISession session = OpenHibernateSession.OpenSession())
+                {
+                    addLocation = session.Get<AddLocation>(Name);
+                }
+                return addLocation;
+            }
+            }
+        }
+}
